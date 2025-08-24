58dcf47c232f7f009063296fe4bf38fd09a41e4f
rakib <rakibislam4913@gmail.com>
2025-08-24 14:34:14 +0600

    Updated login function

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.getElementById('loginButton').addEventListener("click",function(e){
     e.preventDefault();
-    console.log('Login button clicked');
+    const mobileNumber = 11223344
+    const pinNumber = 1234
+
+    const mobileNumberValue = document.getElementById("mobile-number").value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue)
+
+    const pinNumberValue = document.getElementById("pin-number").value
+    const pinNumberValueConverted = parseInt(pinNumberValue)
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+       alert("Login Successful")    
+    } else {
+        // console.log("Login Failed")
+        alert("Invalid mobile number or PIN")
+    }
 })
